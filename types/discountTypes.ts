01a6e181e029dcc68f164220e82c3d70460b41a6
Ivan Garcia <ivan03idk@gmail.com>
2025-03-24 16:05:11 -0600

    Discounts dashboard working and capable of deleting. Add and edit wip

--- types/discountTypes.ts
+++ types/discountTypes.ts
@@ -11,3 +11,15 @@ export interface CategoryDiscount {
   start_date_discount: string;
   end_date_discount: string;
 }
+
+// For use in discounts dashboard in admin panel
+export interface Discount {
+  id_discount: number;
+  id_product?: number;
+  id_category?: number;
+  percent: number;
+  start_date: string;
+  end_date: string;
+  product_name?: string;
+  category_name?: string;
+}
